da53feedd0c12010d4f91e8ee1f21f04736ea781
Cavin Karema <cavin.mulewa.karema@gmail.com>
2024-07-08 16:21:24 +0300

    added pandas module and exercise for workshop

---
+++ 03-etl-pipelines/exercises/lab-coinmarketcap-api/extract_coinmarketcap_api.py
@@ -0,0 +1,13 @@
+# api_key = "b106e423-b757-47e6-87bc-cd8aa5b64386"
+
+# sign up for API key here - https://pro.coinmarketcap.com/
+# https://coinmarketcap.com/api/documentation/v1/#operation/getV2CryptocurrencyInfo
+# https://en.wikipedia.org/wiki/List_of_cryptocurrencies
+
+def assignment():
+    """
+    Task1 - Ingest crypto data table from the wikipedia link
+    Task2 - Get the list of cryptocurrencies from the table
+    Task3 - Use that list to make an API call to obtain cryptocurrency info from the endpoint in the comments above
+    Task4 - Save you data as a parquet file, name your file cryptocurrency_list.parquet"""
+    pass
---
+++ README.md
@@ -0,0 +1,35 @@
+#### About
+
+This is a beginners ETL workshop aimed at equipping the participants with a firm foundation in data warehousing
+
+
+
+The following modules will be covered throughout the entire period of this workshop
+
+1. Introduction to data engineering
+
+2. ETL pipelines
+
+3. Data Warehousing
+
+4. Workflow Orchestration
+   
+   
+
+The following will be used throughout the ETL workshop.
+
+- Pandas
+
+- Postgres
+
+- Snowflake
+
+- Airbyte
+
+- Mage
+
+- psycopg
+
+- sqlalchemy
+
+- 
